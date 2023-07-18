acb0eaa73b01490cd084332e524f40ba79faa5d2
SulRinM <risultan@mail.ru>
2023-07-18 23:49:19 +0300

    Добавьте файлы проекта.

---
+++ CPP_ORM/CPP_ORM.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <Wt/Dbo/Dbo.h>
+#include <Wt/Dbo/backend/Postgres.h>
+
+class Book;
+class Publisher;
+class Book;
+class Stock;
+class Sale;
+
+class Store {
+public:
+	std::string name = "";
+	Wt::Dbo::collection<Wt::Dbo::ptr<Stock>> stocks;
+
+	template<class Action>
+	void persist(Action& a) {
+		Wt::Dbo::field(a, name, "name");
+		Wt::Dbo::hasMany(a, stocks, Wt::Dbo::ManyToOne, "store");
+	}
+};
+
+class Publisher {
+public:
+	std::string name = "";
+	Wt::Dbo::collection<Wt::Dbo::ptr<Book>> books;
+
+	template<class Action>
+	void persist(Action& a) {
+		Wt::Dbo::field(a, name, "name");
+		Wt::Dbo::hasMany(a, books, Wt::Dbo::ManyToOne, "publisher");
+	}
+};
+
+
+class Book {
+public:
+	std::string title = "";
+	Wt::Dbo::ptr<Publisher> publisher;
+	Wt::Dbo::collection<Wt::Dbo::ptr<Stock>> stocks;
+
+	template<class Action>
+	void persist(Action& a) {
+		Wt::Dbo::field(a, title, "title");
+		Wt::Dbo::belongsTo(a, publisher, "publisher");
+		Wt::Dbo::hasMany(a, stocks, Wt::Dbo::ManyToOne, "book");
+	}
+};
+
+
+class Stock {
+public:
+	int count = 0;
+	Wt::Dbo::ptr<Book> book;
+	Wt::Dbo::ptr<Store> store;
+	Wt::Dbo::collection < Wt::Dbo::ptr<Sale> > sales;
+	 
+	template<class Action>
+	void persist(Action& a) {
+		Wt::Dbo::field(a, count, "count");
+		Wt::Dbo::belongsTo(a, book, "book");
+		Wt::Dbo::belongsTo(a, store, "store");
+		Wt::Dbo::hasMany(a, sales, Wt::Dbo::ManyToOne, "stock");
+	}
+};
+
+class Sale {
+public:
+	std::string date;
+	int count;
+	int price;
+	Wt::Dbo::ptr<Stock> stock;
+
+
+	template<class Action>
+	void persist(Action& a) {
+		Wt::Dbo::field(a, date, "date");
+		Wt::Dbo::field(a, price, "price");
+		Wt::Dbo::field(a, count, "count");
+		Wt::Dbo::belongsTo(a, stock, "stock");
+	}
+};
+
+
+int main() {
+	try {
+		std::string connectionString =
+			"host=localhost "
+			" port=5432"
+			" dbname=newDB"
+			" user=postgres"
+			" password=1990";
+		auto postgres = std::make_unique<Wt::Dbo::backend::Postgres>(connectionString);
+		Wt::Dbo::Session session;
+		session.setConnection(std::move(postgres));
+		session.mapClass<Book>("book");
+		session.mapClass<Publisher>("publisher");
+		session.mapClass<Store>("store");
+		session.mapClass<Stock>("stock");
+		session.mapClass<Sale>("sale");
+
+		session.createTables();
+	}
+	catch (const Wt::Dbo::Exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+
+	return 0;
+}
+
